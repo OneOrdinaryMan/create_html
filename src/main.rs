9ba3e2d861565020c4cd660dfde359e3e633d261
One_Ordinary_Man <srivathsansudarsanan@outlook.com>
2023-04-27 20:31:07 +0530

    The env are found and the dir check is done!

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use std::{env, fs, path::Path, process};
+fn main() {
+    let args: Vec<String> = env::args().skip(1).collect();
+    if args.is_empty() {
+        println!("There are no arguments!");
+        process::exit(1);
+    } else if 1 < args.len() {
+        println!("There are more than one arguments");
+    }
+    let home_env = option_env!("HOME");
+    let home_dir = match home_env {
+        Some(value) => String::from(value),
+        None => String::new(),
+    };
+    if home_dir.is_empty() {
+        println!("The env is not set");
+        process::exit(1);
+    }
+    let html_dir = format!("{}{}", &home_dir, "/Development/HTML");
+    let html_path = Path::new(&html_dir);
+    if !html_path.try_exists().unwrap() {
+        println!("The path does'nt exists");
+        process::exit(1);
+    }
+}
