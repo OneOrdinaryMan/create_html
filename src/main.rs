ce320ce516f4e19c0da9b5b523993b740d006f40
One_Ordinary_Man <srivathsansudarsanan@outlook.com>
2023-04-28 08:57:04 +0530

    well writes to a file.but is errror prone

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, fs, path::Path, process};
+use std::{env, fs, io::Write, path::Path, process};
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
     if args.is_empty() {
@@ -6,7 +6,9 @@ fn main() {
         process::exit(1);
     } else if 1 < args.len() {
         println!("There are more than one arguments");
+        process::exit(1);
     }
+    let write_string = String::from("<!Doctype=html>\n<html>\n<head><title>Hello World</title>\n</head>\n<body>Hello World\n</body></html>");
     let home_env = option_env!("HOME");
     let home_dir = match home_env {
         Some(value) => String::from(value),
@@ -23,4 +25,10 @@ fn main() {
         process::exit(1);
     }
     fs::create_dir(&html_path.join(&args[0])).unwrap();
+    let mut file = fs::OpenOptions::new()
+        .create_new(true)
+        .write(true)
+        .open(&html_path.join(args[0].to_owned() + "/index.html"))
+        .unwrap();
+    file.write_all(write_string.as_bytes()).unwrap();
 }
